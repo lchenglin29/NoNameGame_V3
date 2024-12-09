c1dd13e48e6740ca18bc83608dfb7daae4dc3e1e
lchenglin29 <lchenglin29@gmail.com>
2024-12-09 23:27:18 +0800

    update

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ enjoy it:D
 import time
 import curses
 import random
-import copy
+from tqdm import tqdm
 
 RUN = True
 HELP_MSG = """cool
 """
 
-map_by = 1000
-map_bx = 1000
+map_by = 1002
+map_bx = 1002
 render_by = 10
 render_bx = 24
 map_info = {
@@ -41,14 +41,14 @@ def guide(stdscr):
 
 def generate_map(ly,lx):
     map = []
-    for i in range(ly+1):
+    for i in tqdm(range(ly), desc="正在處理地圖"):
         cache =[]
-        if i == 0 or i == ly:
-            for j in range(lx+1):
+        if i == 0 or i == ly-1:
+            for j in range(lx):
                 cache.append("#")
         else:
-            for j in range(lx+1):
-                if j == 0 or j == lx:
+            for j in range(lx):
+                if j == 0 or j == lx-1:
                     cache.append("#")
                 else:
                     cache.append(random.choices(map_info["buildings"], [0.9,0.09,0.01],k=1)[0])
@@ -72,53 +72,58 @@ def render_whole_map(map,stdscr):
     stdscr.refresh()
 
 def render_map(y,x,ly,lx,map,stdscr):
-    n_map = map[:]
+    map = map[:]
     cache = []
     y_s = 0 if y-ly<0 else y-ly
     y_e = len(map) if y+ly > len(map) else y+ly
     x_s = 0 if x-lx<0 else x-lx
     x_e = len(map[0]) if x+lx > len(map[0]) else x+lx
-    n_map[y][x] = 'P'
     for ry in range(y_s,y_e):
         cache2 = []
         for rx in range(x_s,x_e):
-            cache2.append(n_map[ry][rx])
+            cache2.append(map[ry][rx])
         cache.append(cache2)
     render_whole_map(cache,stdscr)
 
 def main(stdscr):
     map = generate_map(map_by,map_bx)
-    stdscr.nodelay(True)
+    #stdscr.nodelay(True)
 
     curses.start_color()
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    y, x = 0, 0
+
+    y, x = 1, 1
+    cache = map[y][x]
     while True:
         stdscr.clear()
         #guide(stdscr)
+        map[y][x] = 'P'
         render_map(y,x,render_by,render_bx,map,stdscr)
-
         key = stdscr.getch()
         if key != -1:
             if key == ord('q') or key == ord('Q'):
                 break
-            elif key == curses.KEY_UP:
-                if y != 1:
-                    y -= 1
-            elif key == curses.KEY_DOWN:
-                if y != len(map)-2:
-                    y += 1
-            elif key == curses.KEY_RIGHT:
-                if x != len(map[0])-2:
-                    x += 1
-            elif key == curses.KEY_LEFT:
-                if x != 1:
-                    x -= 1
-
-        time.sleep(0.01)
+            if key in [curses.KEY_UP,curses.KEY_DOWN,curses.KEY_RIGHT,curses.KEY_LEFT]:
+                map[y][x] = cache
+                if key == curses.KEY_UP:
+                    if y != 1:
+                        y -= 1
+                elif key == curses.KEY_DOWN:
+                    if y != len(map)-2:
+                        y += 1
+                elif key == curses.KEY_RIGHT:
+                    if x != len(map[0])-2:
+                        x += 1
+                elif key == curses.KEY_LEFT:
+                    if x != 1:
+                        x -= 1
+                cache = map[y][x]
+                map[y][x] = 'P'
+        time.sleep(0.03)
 
 
 curses.wrapper(main)
+
